21f04da2ee6d633924af1ff176f5a571be206db2
Yogurt7v <egorov-2k@yandex.ru>
2023-04-16 09:52:02 +0300

    JS practise

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-let a = "5";
-let b = 3;
 
 
-let x = a > b ;
-console.log(x);
+function compare (x, y, n,) {
+    let a = Math.floor((x % Math.floor(x)) * Math.pow(10, n));
+    let b = Math.floor((y % Math.floor(y)) * Math.pow(10, n));
+    console.log ( a == b);
+}
+
+compare (13.1234590, 2.123, 5); //ok
+compare(13.89044, 2.89143, 2); // ok
+compare(13.89044, 2.89143, 3); //ok
